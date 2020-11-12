fb8e910f93b8d58142e1fe49e0551ff498b8f5a0
bnmcgn@gmail.com <bnmcgn@gmail.com>
2020-11-12 10:57:39 -0800

    Def-component forms converted to ReaCL style. def-ps-package started.

--- flaglib.lisp
+++ flaglib.lisp
@@ -2,48 +2,41 @@
 
 (in-package #:flaglib)
 
+(def-ps-package flaglib
+  :ps-requirements (#:paren6 #:reacl #:ps-gadgets #:ps-react-gadgets)
+  :js-requirements '("polyfill" ;; who needs?
+                     ;; "lodash"
+                     ;; "jquery"
+                     ;; "rangy/lib/rangy" ;; Need this?
+                     "rangy/lib/rangy-textrange"
+                     "react-portal-tooltip"
+                     "react-tabs"
+                     "react-on-screen")
+  :ps-files '("displayables.parenscript"
+              "grouped.parenscript"
+              "misc.parenscript"
+              "mood.parenscript"
+              "opinion-page.parenscript"
+              "target-article.parenscript"
+              "target.parenscript"
+              "target-summary.parenscript"
+              "target-thread.parenscript"
+              "things.parenscript"
+              "titlebar.parenscript"))
 
+#|
 
-;;FIXME: react, react-dom should be loaded from the npm bundle.
-(define-default-parts warflagger-base
-  :@javascript-link "https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.26.0/polyfill.js"
-  ;;:@javascript-link "https://unpkg.com/react@16.12.0/umd/react.development.js"
-  ;;:@javascript-link "https://unpkg.com/react-dom@16.12.0/umd/react-dom.development.js"
-  :@javascript-link "/static/javascript/warflagger-bundle.js"
-  :@javascript (ps:ps
-                 (setf -react (require "react"))
-                 (setf (ps:@ -react #:create-class) (require "create-react-class"))
-                 (setf (ps:@ -react -d-o-m) (require "react-dom-factories"))
-                 (setf -redux (require "redux"))
-                 (setf -react-redux (require "react-redux")))
-  ;;:@javascript-link
-  ;;"https://cdnjs.cloudflare.com/ajax/libs/redux/4.0.0/redux.js"
-  ;;:@javascript-link
-  ;;"https://cdnjs.cloudflare.com/ajax/libs/react-redux/5.0.7/react-redux.js"
+def-ps-package should probably have a field for resources such as css
 
-  :@account-info #'account-bar
-  :@javascript-link "/static/javascript/jquery/1.9.1/jquery.js"
-  :@javascript-link  "https://cdn.jsdelivr.net/npm/lodash@4/lodash.min.js"
-  ;;FIXME: Should be able to bundle these with browserify. Can't.
-  :@javascript-link "/static/node_modules/rangy/lib/rangy-core.js"
-  :@javascript-link "/static/node_modules/rangy/lib/rangy-textrange.js"
-  :@javascript-link *warflagger-js-resources*
-  :@head #'favicon-links
-  :@site-index)
-
-
-(define-parts target-parts
   :@css-link "/static/css/target.css"
   ;;FIXME: Need better way to include:
-  :@css-link "/static/css/react-tabs.css")
+  :@css-link "/static/css/react-tabs.css"
 
+|#
 
+;; For target styling
 (defparameter *intensity-thresholds*
   '((0 . 2) (5 . 4) (20 . 6) (50 . 8) (100 . 16)))
-
-
-
-;;Where is this stuff being used?:
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Status indicator for targets
@@ -64,31 +57,5 @@
         :x-problematic "Has conflict"
         :x-unverified "Has unresolved questions"))
 
-
-(defun draw-indicator (category direction text)
-  (html-out
-    (:span :style (format nil "background-color: ~a; margin-left: 1px; margin-right: 1px;"
-                          (getf *direction-colors* direction))
-           (:img :src (format nil "/static/img/~a.svg"
-                              (getf *indicator-names* category))
-                 :title text
-                 :style "height: .75em; :background-color: black;"))))
-
-(defun display-warstats (wstats)
-  (when (<= 1 (car (getf wstats :x-supported)))
-    (draw-indicator :x-supported :positive (getf *warstat-text* :x-supported)))
-  (when (<= 1 (car (getf wstats :x-dissed)))
-    (draw-indicator :x-dissed :negative (getf *warstat-text* :x-dissed)))
-  (when (<= 1 (car (getf wstats :x-right)))
-    (draw-indicator :x-right :positive (getf *warstat-text* :x-right)))
-  (when (<= 1 (car (getf wstats :x-wrong)))
-    (draw-indicator :x-wrong :negative (getf *warstat-text* :x-wrong)))
-  (when (or (<= 1 (car (getf wstats :x-problematic)))
-            (<= 1 (second (getf wstats :x-supported)))
-            (<= 1 (second (getf wstats :x-dissed)))
-            (<= 1 (second (getf wstats :x-right)))
-            (<= 1 (second (getf wstats :x-wrong)))
-            (<= 1 (second (getf wstats :x-unverified))))
-    (draw-indicator :x-problematic :contested (getf *warstat-text* :x-problematic)))
-  (when (<= 1 (car (getf wstats :x-unverified)))
-    (draw-indicator :x-unverified :contested (getf *warstat-text* :x-unverified))))
+;;FIXME: Removing draw-indicator and display-warstats because they render server side. But they
+;; refer to *indicator-names* and *warstat-text* above. Decide how duplicates should be removed.
